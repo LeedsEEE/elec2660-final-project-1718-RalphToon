9192ac61ae6deb2c082b7e8a663b3e2bf655f84e
Ralph Toon <Ralph@Karens-MacBook-Pro.local>
2017-11-08 14:48:27 +0000

    All required view controllers added

---
+++ FinalProject-LOLRankedCompanion/DataInputViewController.h
@@ -0,0 +1,13 @@
+//
+//  DataInputViewController.h
+//  FinalProject-LOLRankedCompanion
+//
+//  Created by Ralph Toon on 08/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataInputViewController : UIViewController
+
+@end
---
+++ FinalProject-LOLRankedCompanion/GameTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameTableViewController.h
+//  FinalProject-LOLRankedCompanion
+//
+//  Created by Ralph Toon on 08/11/2017.
+//  Copyright © 2017 University of Leeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameTableViewController : UITableViewController
+
+@end
